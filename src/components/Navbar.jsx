20777f45f6782ce4843048e7e145ae85d116fb88
Burak Erçıktı <burakercikti@outlook.com>
2024-01-20 14:27:51 +0300

    navbar menu style edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { links } from '../data'
+import "./Navbar.css"
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <nav className="nav">
+      <div className={`${showMenu ? 'nav__menu show-menu' : 'nav__menu'}`}>
+        <ul className="nav__list">
+           {links.map(({ name, icon, path }, index) => {
+
+              return (
+                <li key={index} className="nav__item">
+                  <NavLink            
+                    to={path}
+                    className={({isActive}) => isActive ? 
+                    "nav__link active-nav" : "nav__link"
+                  }
+                  onClick={() => setShowMenu(!showMenu)}
+                  >
+                    {icon}
+                    <h3 className="nav__name">
+                      {name}
+                    </h3>
+                  </NavLink>
+                </li>
+              );
+           })}
+        </ul>
+      </div>
+
+      <div className = {`${showMenu ? 'nav__toggle animate-toggle' : 'nav__toggle'}
+      `}
+      onClick={() => setShowMenu(!showMenu)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
   )
 }
 
